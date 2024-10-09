df11257da102a9e9c56b3f9eb85cb84009611a10
goranikin <goranikin@snu.ac.kr>
2024-10-09 19:10:26 +0900

    useNavigate로 버튼 누르면 url 변경 설정. router 정확히 어케 쓰는 지 모르겠는데... 일단 커밋

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App';
 import { EnvContext } from './context/EnvContext';
@@ -19,9 +19,9 @@ const startApp = () => {
   createRoot(root).render(
     <StrictMode>
       <EnvContext.Provider value={ENV}>
-          <BrowserRouter>
-              <App />
-          </BrowserRouter>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </EnvContext.Provider>
     </StrictMode>,
   );
--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,80 +1,93 @@
-import { useState } from 'react';
-
-import { Login } from './landing-login';
+import { useNavigate } from 'react-router-dom';
 
 export const Landing = () => {
-  const [mode, setMode] = useState<'LOGIN' | 'SIGNUP'>('LOGIN');
+  // const router = createBrowserRouter([
+  //   {
+  //     path: '/',
+  //     element: <LandingMain />,
+  //   },
+  //   {
+  //     path: '/login',
+  //     element: <div></div>, // 로그인 컴포넌트로 교체하기
+  //   },
+  //   {
+  //     path: '/signup',
+  //     element: <div></div> // 회원가입 컴포넌트로 교체하기
+  //   },
+  // ])
 
   return (
-    <div
-      className="p-[100px]"
-      onClick={() => {
-        setMode('SIGNUP');
-      }}
-    >
+    <div className="p-[100px]">
       <LandingMain />
-      <div>
-        {
-          {
-            LOGIN: <Login />,
-            SIGNUP: <div></div>,
-          }[mode]
-        }
-      </div>
     </div>
   );
 };
 
 const LandingMain = () => {
+  const navigate = useNavigate();
+
+  const navigateOnClick = (path: string) => {
+    navigate(path);
+  };
+
   return (
-      <div id="container" className="relative w-[375px] h-[800px] bg-white">
+    <div id="container" className="relative w-[375px] h-[800px] bg-white">
+      <div
+        id="status-line"
+        className="w-full h-11 bg-white flex justify-between items-center font-semibold pl-5"
+      >
+        <p>9:41 AM</p>
+        <img src="/status-icon.png" alt="status-icon" className="h-[44px]" />
+      </div>
+      <div
+        id="wrapper-feature"
+        className="mt-[232px] mb-[90px] w-full h-[434px] bg-white flex flex-col items-center"
+      >
+        <img
+          id="image-TimeTable"
+          src={`/TimeTable.png`}
+          alt="timetable"
+          className="w-[108px] h-[101px] mb-[136px]"
+        ></img>
         <div
-            id="status-line"
-            className="w-full h-11 bg-white flex justify-between items-center font-semibold pl-5"
+          id="wrapper-button"
+          className="w-[311px] h-[72px] mb-[40px] gap-[15px] flex flex-col justify-center items-center"
         >
-          <p>9:41 AM</p>
-          <img src="/status-icon.png" alt="status-icon" className="h-[44px]"/>
+          <button
+            id="login"
+            className="w-[311px] h-[41px] bg-[#F58D3D] text-white"
+            onClick={() => {
+              navigateOnClick('/login');
+            }}
+          >
+            로그인
+          </button>
+          <button
+            id="signup"
+            className="w-[311px] h-[17px]"
+            onClick={() => {
+              navigateOnClick('/signup');
+            }}
+          >
+            회원가입
+          </button>
         </div>
         <div
-            id="wrapper-feature"
-            className="mt-[232px] mb-[90px] w-full h-[434px] bg-white flex flex-col items-center"
+          id="wrapper-SNS"
+          className="w-full h-[85px] flex flex-col justify-center items-center gap-[24px]"
         >
-          <img
-              id="image-TimeTable"
-              src={`/TimeTable.png`}
-              alt="timetable"
-              className="w-[108px] h-[101px] mb-[136px]"
-          ></img>
+          <p>SNS 계정으로 계속하기</p>
           <div
-              id="wrapper-button"
-              className="w-[311px] h-[72px] mb-[40px] gap-[15px] flex flex-col justify-center items-center"
+            id="SNS-icon"
+            className="h-[44px] gap-[12px] flex justify-center"
           >
-            <button
-                id="login"
-                className="w-[311px] h-[41px] bg-[#F58D3D] text-white"
-            >
-              로그인
-            </button>
-            <button id="signup" className="w-[311px] h-[17px]">
-              회원가입
-            </button>
-          </div>
-          <div
-              id="wrapper-SNS"
-              className="w-full h-[85px] flex flex-col justify-center items-center gap-[24px]"
-          >
-            <p>SNS 계정으로 계속하기</p>
-            <div
-                id="SNS-icon"
-                className="h-[44px] gap-[12px] flex justify-center"
-            >
-              <img src="/kakaotalkid.png" alt="kakaotalk"/>
-              <img src="/googleid.png" alt="google"/>
-              <img src="/facebookid.png" alt="facebook"/>
-              <img src="/appleid.png" alt="apple"/>
-            </div>
+            <img src="/kakaotalkid.png" alt="kakaotalk" />
+            <img src="/googleid.png" alt="google" />
+            <img src="/facebookid.png" alt="facebook" />
+            <img src="/appleid.png" alt="apple" />
           </div>
         </div>
       </div>
+    </div>
   );
 };
