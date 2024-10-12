f3554d466e1f3e871fcc23f1ca977b969d70712f
Yeonu-Kim <ywk0524@snu.ac.kr>
2024-10-12 17:25:19 +0900

    [feat] Add global font

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,6 +38,21 @@ export default {
         popup: 'popup 0.3s ease-out',
       },
     },
+    fontFamily: {
+      sans: [
+        'Pretendard',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+      ],
+    },
   },
   plugins: [],
 };
