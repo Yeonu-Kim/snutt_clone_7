fdedd4425bb63190045637d285e66581eb6891bd
Yeonu-Kim <ywk0524@snu.ac.kr>
2024-10-13 08:14:02 +0900

    [style] Rearrange the main page elements

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -9,52 +9,59 @@ export const LandingPage = () => {
       <div id="container" className="relative h-full bg-white">
         <div
           id="wrapper-feature"
-          className="mt-[232px] mb-[90px] w-full h-[434px] bg-white flex flex-col items-center"
+          className="flex relative w-full h-full flex-col items-center justify-center gap-[136px]"
         >
-          <img
-            id="image-TimeTable"
-            src="/logo/TimeTable.png"
-            alt="timetable"
-            className="w-[108px] h-[101px] mb-[136px]"
-          ></img>
-          <div
-            id="wrapper-button"
-            className="w-[311px] h-[72px] mb-[40px] gap-[15px] flex flex-col justify-center items-center"
-          >
-            <button
-              id="login"
-              className="w-[311px] h-[41px] bg-[#F58D3D] text-white hover:bg-[#E07C2C] transition-colors duration-200 rounded-md"
-              onClick={toSignIn}
-            >
-              로그인
-            </button>
-            <button
-              id="signup"
-              className="w-[311px] h-[17px] hover:text-[#F58D3D] transition-colors duration-300"
-              onClick={toSignUp}
-            >
-              회원가입
-            </button>
+          <div className="imageWrapper flex flex-col items-center gap-4">
+            <img
+              id="image-TimeTable"
+              src="/logo/snutt_logo.svg"
+              alt="timetable"
+              className="w-[60px] h-[60px] "
+            ></img>
+            <h1 className="title font-extrabold font-[eng] text-2xl">
+              TimeTable
+            </h1>
           </div>
-          <div
-            id="wrapper-SNS"
-            className="w-full h-[85px] flex flex-col justify-center items-center gap-[24px]"
-          >
-            <div className="flex justify-center items-center w-[320px]">
-              <div className="flex-grow border-t border-gray-300 mr-2" />
-              <p className="text-[#C4C4C4] text-sm flex items-center">
-                SNS 계정으로 계속하기
-              </p>
-              <div className="flex-grow border-t border-gray-300 ml-2" />
+          <div className="bottomWrapper flex-col">
+            <div
+              id="wrapper-button"
+              className="w-full h-[72px] mb-[40px] gap-[15px] flex flex-col justify-center items-center"
+            >
+              <button
+                id="login"
+                className="w-[311px] h-[41px] bg-[#F58D3D] text-white hover:bg-[#E07C2C] transition-colors duration-200 rounded-md"
+                onClick={toSignIn}
+              >
+                로그인
+              </button>
+              <button
+                id="signup"
+                className="w-[311px] h-[17px] hover:text-[#F58D3D] transition-colors duration-300"
+                onClick={toSignUp}
+              >
+                회원가입
+              </button>
             </div>
             <div
-              id="SNS-icon"
-              className="h-[44px] gap-[12px] flex justify-center"
+              id="wrapper-SNS"
+              className="w-full h-[85px] flex flex-col justify-center items-center gap-[24px]"
             >
-              <img src="/landing/kakaotalkid.png" alt="kakaotalk" />
-              <img src="/landing/googleid.png" alt="google" />
-              <img src="/landing/facebookid.png" alt="facebook" />
-              <img src="/landing/appleid.png" alt="apple" />
+              <div className="flex justify-center items-center w-[320px]">
+                <div className="flex-grow border-t border-gray-300 mr-2" />
+                <p className="text-[#C4C4C4] text-sm flex items-center">
+                  SNS 계정으로 계속하기
+                </p>
+                <div className="flex-grow border-t border-gray-300 ml-2" />
+              </div>
+              <div
+                id="SNS-icon"
+                className="h-[44px] gap-[12px] flex justify-center"
+              >
+                <img src="/landing/kakaotalkid.png" alt="kakaotalk" />
+                <img src="/landing/googleid.png" alt="google" />
+                <img src="/landing/facebookid.png" alt="facebook" />
+                <img src="/landing/appleid.png" alt="apple" />
+              </div>
             </div>
           </div>
         </div>
