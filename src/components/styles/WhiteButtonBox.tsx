37b4012bcd3d3c14b7945144c6ee296032fcb3d0
goranikin <goranikin@snu.ac.kr>
2024-10-26 00:39:40 +0900

    WhiteButtonBox.tsx 작성 (마이페이지에서 꾸미기 용으로 여러 개 넣을 거 예상)

--- src/components/styles/WhiteButtonBox.tsx
+++ src/components/styles/WhiteButtonBox.tsx
@@ -9,11 +9,13 @@ type WhiteButtonBoxProps = {
 export const WhiteButtonBox = ({
   children,
   className = '',
+  onClick,
 }: WhiteButtonBoxProps) => {
   return (
     <button
       className={`bg-white w-[335px] h-10 rounded-lg
               cursor-pointer hover:bg-gray-100 transition-colors duration-200 ${className}`}
+      onClick={onClick}
     >
       {children}
     </button>
