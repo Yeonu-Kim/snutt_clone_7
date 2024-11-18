bccafcef4d5bda7dbb8c863a308fce209b573888
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2024-11-19 02:40:48 +0900

    [feat] state 관리 추가

--- src/app_pages/Lecture/CreateLecture/index.tsx
+++ src/app_pages/Lecture/CreateLecture/index.tsx
@@ -1,21 +1,73 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 import { BottomSheetContainer } from '@/components/BottomeSheetContainer';
+import { SpinnerLoading } from '@/components/Loading';
+import { ServiceContext } from '@/context/ServiceContext';
+import { TokenAuthContext } from '@/context/TokenAuthContext';
+import { type CustomLecture } from '@/entities/lecture';
+import { dayMap, minToTime } from '@/entities/time';
+import { useGuardContext } from '@/hooks/useGuardContext';
+import { useBottomSheet } from '@/hooks/useVisible';
+import { showDialog } from '@/utils/showDialog';
 
 export const AddCustomTimeTable = ({ onClose }: { onClose: () => void }) => {
+  const { timetableId } = useParams();
+  const { isVisible, handleClose } = useBottomSheet({ onClose });
+  const { createCustomLecture, isPending } = useCreateCustomLecture({
+    handleClose,
+  });
+  const [courseTitle, setCourseTitle] = useState('새로운 강의');
+  const [instructor, setInstructor] = useState('');
+  const [credit, setCredit] = useState(0);
+  const [color, setColor] = useState(0);
+  const [remark, setRemark] = useState('');
+  const [place, setPlace] = useState('');
+  const [is_forced, setIsForced] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (timetableId === undefined) {
+      throw new Error('시간표 아이디가 존재하지 않습니다.');
+    }
+    const lectureDetails = {
+      course_title: courseTitle,
+      instructor: instructor,
+      credit: credit,
+      class_time_json: [
+        {
+          day: dayMap['Wed'] ?? 2,
+          startMinute: 1140,
+          endMinute: 1230,
+          place: place,
+          start_time: minToTime(1140),
+          end_time: minToTime(1230),
+        },
+      ],
+      remark: remark,
+      colorIndex: color,
+      is_forced: is_forced,
+    };
+
+    createCustomLecture({
+      timetableId: timetableId,
+      lectureDetails: lectureDetails,
+    });
+  };
+
   return (
     <BottomSheetContainer
-      isVisible={true}
-      onClick={onClose}
+      isVisible={isVisible}
+      onClick={handleClose}
       bgColor="bg-gray-100"
       heightClass="h-[98%]"
     >
-      <form className="Wrapper">
-        <div className="Button Wrapper rounded-t-xl bg-white flex justify-between items-center px-4 py-3 border-b mb-4 ">
-          <button onClick={onClose} className="Cancel_Button">
-            취소
-          </button>
-          <button type="submit" className="Save_Button">
-            저장
-          </button>
+      {isPending && <SpinnerLoading />}
+      <form onSubmit={handleSubmit}>
+        <div className="rounded-t-xl bg-white flex justify-between items-center px-4 py-3 border-b mb-4 ">
+          <button onClick={handleClose}>취소</button>
+          <button type="submit">저장</button>
         </div>
         <div className="Class_Info bg-white p-4 space-y-4 mb-4">
           <div className="grid grid-cols-12 gap-2 items-center">
@@ -25,7 +77,10 @@ export const AddCustomTimeTable = ({ onClose }: { onClose: () => void }) => {
             <input
               type="text"
               className="Class_Name_Input col-span-9 focus:outline-none focus:ring-0"
-              value="새로운 강의"
+              value={courseTitle}
+              onChange={(e) => {
+                setCourseTitle(e.target.value);
+              }}
             />
           </div>
           <div className="grid grid-cols-12 gap-2 items-center">
@@ -36,6 +91,10 @@ export const AddCustomTimeTable = ({ onClose }: { onClose: () => void }) => {
               type="text"
               className="Professor_Input col-span-9 focus:outline-none focus:ring-0"
               placeholder="(없음)"
+              value={instructor}
+              onChange={(e) => {
+                setInstructor(e.target.value);
+              }}
             />
           </div>
           <div className="grid grid-cols-12 gap-2 items-center">
@@ -45,7 +104,10 @@ export const AddCustomTimeTable = ({ onClose }: { onClose: () => void }) => {
             <input
               type="text"
               className="Credit_Input col-span-9 focus:outline-none focus:ring-0"
-              value={0}
+              value={credit}
+              onChange={(e) => {
+                setCredit(Number(e.target.value));
+              }}
             />
           </div>
           <div className="grid grid-cols-12 gap-2 items-center">
@@ -55,7 +117,10 @@ export const AddCustomTimeTable = ({ onClose }: { onClose: () => void }) => {
             <input
               type="color"
               className="Color_Input col-span-9"
-              value="#000000"
+              value={color}
+              onChange={(e) => {
+                setColor(Number(e.target.value));
+              }}
             />
           </div>
         </div>
@@ -67,6 +132,10 @@ export const AddCustomTimeTable = ({ onClose }: { onClose: () => void }) => {
             type="text"
             className="Notes_Input col-span-9 focus:outline-none focus:ring-0"
             placeholder="(없음)"
+            value={remark}
+            onChange={(e) => {
+              setRemark(e.target.value);
+            }}
           />
         </div>
         <div className="Time_Place flex flex-col bg-white p-4 space-y-4">
@@ -78,11 +147,7 @@ export const AddCustomTimeTable = ({ onClose }: { onClose: () => void }) => {
               <label className="Time_Label col-span-3 text-gray-600 text-sm ">
                 시간
               </label>
-              <input
-                type="time"
-                className="Time_Input col-span-9"
-                value="Wed 19:00 ~ 20:30 "
-              />
+              <div className="Time_Input col-span-9">Wed 19:00 ~ 20:30</div>
             </div>
             <div className="Place grid grid-cols-12 gap-2 items-center">
               <label className="Place_Label col-span-3 text-gray-600 text-sm ">
@@ -92,6 +157,10 @@ export const AddCustomTimeTable = ({ onClose }: { onClose: () => void }) => {
                 type="text"
                 className="Place_Input col-span-9 focus:outline-none focus:ring-0"
                 placeholder="(없음)"
+                value={place}
+                onChange={(e) => {
+                  setPlace(e.target.value);
+                }}
               />
             </div>
           </div>
@@ -102,4 +171,48 @@ export const AddCustomTimeTable = ({ onClose }: { onClose: () => void }) => {
       </form>
     </BottomSheetContainer>
   );
+};
+
+const useCreateCustomLecture = ({
+  handleClose,
+}: {
+  handleClose: () => void;
+}) => {
+  const { lectureService } = useGuardContext(ServiceContext);
+  const { token } = useGuardContext(TokenAuthContext);
+  const { showErrorDialog } = showDialog();
+  const queryClient = useQueryClient();
+
+  const { mutate: createCustomLecture, isPending } = useMutation({
+    mutationFn: async ({
+      lectureDetails,
+      timetableId,
+    }: {
+      lectureDetails: CustomLecture;
+      timetableId: string;
+    }) => {
+      if (token === null) {
+        throw new Error('토큰이 존재하지 않습니다.');
+      }
+      return lectureService.createCustomLecture({
+        token,
+        timetableId,
+        lectureDetails,
+      });
+    },
+    onSuccess: async (response) => {
+      if (response.type === 'success') {
+        handleClose();
+        await queryClient.invalidateQueries({
+          queryKey: ['LectureService'],
+        });
+      } else {
+        showErrorDialog(response.message);
+      }
+    },
+    onError: () => {
+      showErrorDialog('강의 생성 중 문제가 발생했습니다.');
+    },
+  });
+  return { createCustomLecture, isPending };
 };
